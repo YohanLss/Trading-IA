1d0ef6be4d3309309535ce6556326827af83b176
Ben Ollomo <benji.ollomo@gmail.com>
2025-11-15 17:06:46 -0500

    Re-enabled and configured NLTK for Cloud Function pipeline:
    - Added `nltk` import and configured custom data path using `os`.
    - Updated `main_cloud_functions.py` to include NLTK initialization for processing requirements.

--- main_cloud_functions.py
+++ main_cloud_functions.py
@@ -1,6 +1,7 @@
 from pipelines import ArticlePipelineController
 from utils.logger import logger
-
+import nltk
+import os
 logger.setLevel("DEBUG")
 
 pipeline = ArticlePipelineController(article_limit=20, verify_db=True)
@@ -10,6 +11,9 @@ def run_article_fetch_pipeline(request):
     """
        HTTP Cloud Function entrypoint
     """
+    NLTK_DIR = os.path.join(os.path.dirname(__file__), "nltk_data")
+    nltk.data.path.append(NLTK_DIR)
+    
     logger.info("Cloud Function triggered to run article pipeline.")
     try:
         pipeline.run()
@@ -20,5 +24,5 @@ def run_article_fetch_pipeline(request):
         return f"error: {e}", 500
 
 if __name__ == "__main__":
-
+    
     run_article_fetch_pipeline(None)
